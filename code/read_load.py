4050411d47929b875895ed13c0562d5685571a32
Zechen Zhang <fluffyzed2@gmail.com>
2023-04-11 14:10:38 -0400

    Find out the video file with large error

--- code/read_load.py
+++ code/read_load.py
@@ -3,19 +3,25 @@ import pandas as pd
 import numpy as np
 
 if __name__ == "__main__":
-    # dir_path = "../Phase1_data/Videos/train-001_of_002"
-    # res = []
-    #
-    # df_MAE = pd.DataFrame()
-    #
-    # for path in os.listdir(dir_path):
-    #     if os.path.isfile(os.path.join(dir_path, path)):
-    #         res.append(path)
-    # print(res[0])
+    dir_path = "../../Phase1_data/Videos/train-001_of_002"
+    res = []
+
+    df_MAE = pd.DataFrame()
+
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            res.append(path)
+    print(res[0])
+
     MAE = np.loadtxt("../MAE_001.txt")
     print("Total number of frame:", len(MAE))
     src_error = []
     for i in range(len(MAE)):
-        if MAE[i] > 25:
+        if MAE[i] > 30:
             src_error.append(i)
-    print(len(src_error))
+    src_error = np.array(src_error)
+    print(src_error)
+    print("Number of large error file:", len(src_error))
+    print("Video with large error: ")
+    for file in src_error:
+        print(res[file])
