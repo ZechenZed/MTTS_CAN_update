57b8c9aa93152805a3512faec6aea9e424fa8897
Zechen Zhang <fluffyzed2@gmail.com>
2023-04-11 12:11:47 -0400

    MAE file update and add another file for reading error

--- code/read_load.py
+++ code/read_load.py
@@ -1,11 +1,21 @@
 import os
 import pandas as pd
+import numpy as np
 
-
-dir_path = "../Phase1_data/Videos/train-001_of_002"
-res = []
-
-for path in os.listdir(dir_path):
-    if os.path.isfile(os.path.join(dir_path, path)):
-        res.append(path)
-print(res)
+if __name__ == "__main__":
+    # dir_path = "../Phase1_data/Videos/train-001_of_002"
+    # res = []
+    #
+    # df_MAE = pd.DataFrame()
+    #
+    # for path in os.listdir(dir_path):
+    #     if os.path.isfile(os.path.join(dir_path, path)):
+    #         res.append(path)
+    # print(res[0])
+    MAE = np.loadtxt("../MAE_001.txt")
+    print("Total number of frame:", len(MAE))
+    src_error = []
+    for i in range(len(MAE)):
+        if MAE[i] > 25:
+            src_error.append(i)
+    print(len(src_error))
