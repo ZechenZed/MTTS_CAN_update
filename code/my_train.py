b1445d84c5a095d2620a9fe708cb287c39b9d885
ZechenZed <fluffyzed2@gmail.com>
2023-06-12 11:21:41 -0400

    My training method update

--- code/my_train.py
+++ code/my_train.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] ="7"
+# os.environ["CUDA_VISIBLE_DEVICES"] ="7"
 
 import cv2
 import glob
@@ -10,6 +10,7 @@ from statistics import mean
 from joblib import Parallel, delayed
 import matplotlib
 import matplotlib.pyplot as plt
+from scipy.signal import medfilt
 
 # matplotlib.use('TkAgg', force=True)
 import tensorflow as tf
@@ -28,19 +29,19 @@ from inference_preprocess import preprocess_raw_video
 from model import MTTS_CAN
 
 
-def data_processing(data_type):
-    # Path setting
-    # video_train_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/train/"
-    # video_valid_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/valid/"
-    # video_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/Videos/test/"
-    # BP_phase1_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Ground_truth/BP_raw_1KHz/"
-    # BP_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/blood_pressure/test_set_bp/"
-
-    video_train_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/train/"
-    video_valid_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/valid/"
-    video_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/Videos/test/"
-    BP_phase1_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Ground_truth/BP_raw_1KHz/"
-    BP_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/blood_pressure/test_set_bp/"
+def data_processing_1(data_type, device_type):
+    if device_type == "local":
+        video_train_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/train/"
+        video_valid_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/valid/"
+        video_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/Videos/test/"
+        BP_phase1_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Ground_truth/BP_raw_1KHz/"
+        BP_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/blood_pressure/test_set_bp/"
+    else:
+        video_train_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/train/"
+        video_valid_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/valid/"
+        video_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/Videos/test/"
+        BP_phase1_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Ground_truth/BP_raw_1KHz/"
+        BP_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/blood_pressure/test_set_bp/"
 
     if data_type == "train":
         # Video path reading
@@ -51,14 +52,15 @@ def data_processing(data_type):
         num_video = len(train_videos)
         print(num_video)
 
-        # # Video processing
-        # videos = [Parallel(n_jobs=4)(
-        #     delayed(preprocess_raw_video)(video_train_path + video) for video in train_videos)]
-        # videos = videos[0]
-
         videos = []
-        for video in train_videos:
-            videos.append(preprocess_raw_video(video_train_path + video))
+        if device_type == "local":
+            # Video processing
+            videos = [Parallel(n_jobs=6)(
+                delayed(preprocess_raw_video)(video_train_path + video) for video in train_videos)]
+            videos = videos[0]
+        else:
+            for video in train_videos:
+                videos.append(preprocess_raw_video(video_train_path + video))
 
         tt_frame = 0
         for i in range(num_video):
@@ -84,9 +86,13 @@ def data_processing(data_type):
             BP_lf[frame_ind:frame_ind + cur_frames] = temp_lf
             frame_ind += cur_frames
 
-        # Saving processed frames
-        np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_frames.npy', frames)
-        np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_BP.npy', BP_lf)
+        if device_type == "remote":
+            # Saving processed frames
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_frames.npy', frames)
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_BP.npy', BP_lf)
+        else:
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames.npy', frames)
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP.npy', BP_lf)
     elif data_type == "test":
         # Video path reading
         test_videos = []
@@ -96,14 +102,15 @@ def data_processing(data_type):
         num_video = len(test_videos)
         # print(num_video,train_videos)
 
-        # # Video Preprocessing
-        # videos = [Parallel(n_jobs=-1)(
-        #     delayed(preprocess_raw_video)(video_test_path + video) for video in test_videos)]
-        # videos = videos[0]
-
         videos = []
-        for video in test_videos:
-            videos.append(preprocess_raw_video(video_test_path + video))
+        if device_type == "local":
+            # Video Preprocessing
+            videos = [Parallel(n_jobs=6)(
+                delayed(preprocess_raw_video)(video_test_path + video) for video in test_videos)]
+            videos = videos[0]
+        else:
+            for video in test_videos:
+                videos.append(preprocess_raw_video(video_test_path + video))
 
         tt_frame = 0
         for i in range(num_video):
@@ -130,19 +137,160 @@ def data_processing(data_type):
             frame_ind += cur_frames
 
         # Save the preprocessed frames
-        np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_frames.npy', frames)
-        np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_BP.npy', BP_lf)
+        if device_type == "remote":
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_frames.npy', frames)
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_BP.npy', BP_lf)
+        else:
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames.npy', frames)
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP.npy', BP_lf)
     else:
         print("Please enter the correct datatype")
 
 
-def model_train(exp_type):
-    if exp_type == "train":
-        frames = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/train_frames.npy')
-        BP_lf = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/train_BP.npy')
+def data_processing_2(data_type, device_type):
+    if device_type == "local":
+        video_train_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/train/"
+        video_valid_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Videos/valid/"
+        video_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/Videos/test/"
+        BP_phase1_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase1_data/Ground_truth/BP_raw_1KHz/"
+        BP_test_path = "C:/Users/Zed/Desktop/Project-BMFG/Phase2_data/blood_pressure/test_set_bp/"
     else:
-        frames = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/test_frames.npy')
-        BP_lf = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/test_BP.npy')
+        video_train_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/train/"
+        video_valid_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Videos/valid/"
+        video_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/Videos/test/"
+        BP_phase1_path = "../../../../edrive2/zechenzh/V4V/Phase1_data/Ground_truth/BP_raw_1KHz/"
+        BP_test_path = "../../../../edrive2/zechenzh/V4V/Phase2_data/blood_pressure/test_set_bp/"
+
+    if data_type == "train":
+        # Video path reading
+        train_videos = []
+        for path in sorted(os.listdir(video_train_path)):
+            if os.path.isfile(os.path.join(video_train_path, path)):
+                train_videos.append(path)
+        train_videos = train_videos[0:2]
+        num_video = len(train_videos)
+        print(num_video)
+
+        videos = []
+        if device_type == "local":
+            # Video processing
+            videos = [Parallel(n_jobs=6)(
+                delayed(preprocess_raw_video)(video_train_path + video) for video in train_videos)]
+            videos = videos[0]
+        else:
+            for video in train_videos:
+                videos.append(preprocess_raw_video(video_train_path + video))
+
+        # BP path reading
+        BP_train_path = []
+        for path in sorted(os.listdir(BP_phase1_path)):
+            if os.path.isfile(os.path.join(BP_phase1_path, path)):
+                BP_train_path.append(path)
+
+        BP_lf = []
+        tt_frame = 0
+        frame_video = []
+        for i in range(num_video):
+            BP_temp = np.loadtxt(BP_phase1_path + BP_train_path[i])
+            cur_frames = BP_temp.shape[0] // 1000 * 1000
+            frame_video.append(cur_frames)
+            tt_frame += cur_frames
+            BP_temp_med = medfilt(BP_temp[0:cur_frames])
+            for ind, element in enumerate(BP_temp_med):
+                BP_lf.append(element)
+        BP_lf = np.array(BP_lf)
+
+        # BP & Video frame processing
+        frames = np.zeros(shape=(tt_frame, 36, 36, 6))
+
+        frame_ind = 0
+
+        for i in range(num_video):
+            temp_video = videos[i]
+            cur_frames = frame_video[i]
+            temp_video_expand = np.zeros(shape=(cur_frames, 36, 36, 6))
+            for j in range(0, int(cur_frames / 40)):
+                temp_video_expand[40 * j:40 * (j + 1), :, :, :] = temp_video[j, :, :, :]
+            frames[frame_ind:frame_ind+cur_frames] = temp_video_expand
+        print(frames[0:40][0][0][0])
+
+        # if device_type == "remote":
+        # # Saving processed frames
+        #     np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_frames.npy', frames)
+        #     np.save('../../../../edrive2/zechenzh/preprocessed_v4v/train_BP.npy', BP_lf)
+        # else:
+        #     np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames.npy', frames)
+        #     np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP.npy', BP_lf)
+    elif data_type == "test":
+        # Video path reading
+        test_videos = []
+        for path in sorted(os.listdir(video_test_path)):
+            if os.path.isfile(os.path.join(video_test_path, path)):
+                test_videos.append(path)
+        num_video = len(test_videos)
+        # print(num_video,train_videos)
+
+        videos = []
+        if device_type == "local":
+            # Video Preprocessing
+            videos = [Parallel(n_jobs=6)(
+                delayed(preprocess_raw_video)(video_test_path + video) for video in test_videos)]
+            videos = videos[0]
+        else:
+            for video in test_videos:
+                videos.append(preprocess_raw_video(video_test_path + video))
+
+        tt_frame = 0
+        for i in range(num_video):
+            tt_frame += videos[i].shape[0] // 10 * 10
+
+        # BP path reading
+        BP_test = []
+        for path in sorted(os.listdir(BP_test_path)):
+            if os.path.isfile(os.path.join(BP_test_path, path)):
+                BP_test.append(path)
+
+        # BP & Video frames processing
+        frames = np.zeros(shape=(tt_frame, 36, 36, 6))
+        BP_lf = np.zeros(shape=tt_frame)
+        frame_ind = 0
+        for j in range(num_video):
+            temp = np.loadtxt(BP_test_path + BP_test[j])
+            cur_frames = videos[j].shape[0] // 10 * 10
+            temp_lf = np.zeros(cur_frames)
+            frames[frame_ind:frame_ind + cur_frames, :, :, :] = videos[j][0:cur_frames, :, :, :]
+            for i in range(0, cur_frames):
+                temp_lf[i] = mean(temp[i * 40:(i + 1) * 40])
+            BP_lf[frame_ind:frame_ind + cur_frames] = temp_lf
+            frame_ind += cur_frames
+
+        # Save the preprocessed frames
+        if device_type == "remote":
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_frames.npy', frames)
+            np.save('../../../../edrive2/zechenzh/preprocessed_v4v/test_BP.npy', BP_lf)
+        else:
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames.npy', frames)
+            np.save('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP.npy', BP_lf)
+    else:
+        print("Please enter the correct datatype")
+
+
+def model_train(data_type, device_type, task_num, nb_filters1, nb_filters2, dropout_rate1, dropout_rate2, nb_dense):
+    if device_type == "local":
+        if data_type == "train":
+            frames = np.load(
+                'C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames_' + str(task_num) + '.npy')
+            BP_lf = np.load('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP_' + str(task_num) + '.npy')
+        else:
+            frames = np.load('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/test_frames_' + str(task_num) + '.npy')
+            BP_lf = np.load('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/test_BP_' + str(task_num) + '.npy')
+    else:
+        if data_type == "train":
+            frames = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/train_frames.npy')
+            BP_lf = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/train_BP.npy')
+        else:
+            frames = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/test_frames.npy')
+            BP_lf = np.load('../../../../edrive2/zechenzh/preprocessed_v4v/test_BP.npy')
     # Train 132505 * 6
     # frames = frames[132505*5:132505*6]
     # BP_lf = BP_lf[132505*5:132505*6]
@@ -165,30 +313,35 @@ def model_train(exp_type):
     print('Using MTTS_CAN!')
 
     # Create a callback that saves the model's weights
-    model = MTTS_CAN(frame_depth, args.nb_filters1, args.nb_filters2, input_shape,
+    model = MTTS_CAN(frame_depth, nb_filters1, args.nb_filters2, input_shape,
                      dropout_rate1=args.dropout_rate1, dropout_rate2=args.dropout_rate2, nb_dense=args.nb_dense)
     losses = tf.keras.losses.MeanAbsoluteError()
     loss_weights = {"output_1": 1.0}
     opt = "adadelta"
     model.compile(loss=losses, loss_weights=loss_weights, optimizer=opt)
-    if exp_type == "test":
-        model.load_weights('../checkpoints/my_mtts.hdf5')
+    if data_type == "test":
+        model.load_weights('C:/Users/Zed/Desktop/Project-BMFG/BMFG/checkpoints/my_mtts.hdf5')
         model.evaluate(x=(frames[:, :, :, :3], frames[:, :, :, -3:]), y=BP_lf, batch_size=32)
     else:
-        save_best_callback = ModelCheckpoint(filepath="../checkpoints/my_mtts.hdf5", save_best_only=True, verbose=1)
+        if os.listdir("C:/Users/Zed/Desktop/Project-BMFG/BMFG/checkpoints/"):
+            print("Continue training")
+            model.load_weights('C:/Users/Zed/Desktop/Project-BMFG/BMFG/checkpoints/my_mtts.hdf5')
+        save_best_callback = ModelCheckpoint(filepath="C:/Users/Zed/Desktop/Project-BMFG/BMFG/checkpoints/my_mtts.hdf5"
+                                             , save_best_only=True, verbose=1)
         # early_stop = tf.keras.callbacks.EarlyStopping(monitor=losses, patience=10)
-        history = model.fit(x=(frames[:, :, :, :3], frames[:, :, :, -3:]), y=BP_lf, validation_split=0.2,
-                            batch_size=128,
+        history = model.fit(x=(frames[:, :, :, :3], frames[:, :, :, -3:]), y=BP_lf, batch_size=32, validation_split=0.1,
                             epochs=20, callbacks=[save_best_callback], verbose=1, shuffle=False)
 
 
 if __name__ == "__main__":
     # args
     parser = argparse.ArgumentParser()
-    parser.add_argument('-exp', '--exp_type', type=str, default='model',
+    parser.add_argument('-exp', '--exp_type', type=str, default='video',
                         help='experiment type: model or video')
-    parser.add_argument('-data', '--data_type', type=str, default='test',
+    parser.add_argument('-data', '--data_type', type=str, default='train',
                         help='data type')
+    parser.add_argument('-device', '--device_type', type=str, default='local',
+                        help='device type')
     parser.add_argument('-t', '--task', type=int, default=0,
                         help='the order of exp')
     parser.add_argument('-i', '--data_dir', type=str,
@@ -218,7 +371,16 @@ if __name__ == "__main__":
     # tf.keras.backend.clear_session()
     # strategy = tf.distribute.MirroredStrategy()
     # print('Number of devices: {}'.format(strategy.num_replicas_in_sync)
+
+    # test_frames = np.load('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_frames.npy')
+    # test_BP = np.load('C:/Users/Zed/Desktop/Project-BMFG/preprocessed_v4v/train_BP.npy')
+    # for i in range(4):
+    #     np.save('C:/Users/Zed/Desktop\Project-BMFG/preprocessed_v4v/train_frames_' + str(i) + '.npy', test_frames[132505 * i:132505 * (i + 1)])
+    #     np.save('C:/Users/Zed/Desktop\Project-BMFG/preprocessed_v4v/train_BP_' + str(i) + '.npy', test_BP[132505 * i:132505 * (i + 1)])
+
     if args.exp_type == "model":
-        model_train(exp_type=args.exp_type)
+        model_train(data_type=args.data_type, device_type=args.device_type,
+                    task_num=3, nb_filters1=args.nb_filters1, nb_filters2=args.nb_filters2,
+                    dropout_rate1=args.dropout_rate1, dropout_rate2=args.dropout_rate2, nb_dense=args.nb_dense)
     else:
-        data_processing(data_type=args.data_type)
+        data_processing_2(data_type=args.data_type, device_type=args.device_type)
