94c2911c68e760838dbcf645f8b18ca1ae27e903
ZechenZed <fluffyzed2@gmail.com>
2023-06-21 19:28:12 -0400

    updated the facial cropping tech

--- code/my_train.py
+++ code/my_train.py
@@ -202,7 +202,7 @@ def data_processing_3(data_type, device_type, dim=48):
     for path in sorted(os.listdir(video_folder_path)):
         if os.path.isfile(os.path.join(video_folder_path, path)):
             video_file_path.append(path)
-    video_file_path = video_file_path[0:2]
+    # video_file_path = video_file_path[0:2]
     num_video = len(video_file_path)
     print(num_video)
 
