7de100e3249647631017f0c558165431bf18035e
ZechenZed <fluffyzed2@gmail.com>
2023-07-03 19:41:53 -0400

    delete 2 dropout layers

--- code/my_predict.py
+++ code/my_predict.py
@@ -52,63 +52,65 @@ def my_predict(data_type, dataset_type, kernal):
     img_rows = 48
     img_cols = 48
     frame_depth = 1
-    glob_path = 'C:/Users/Zed/Desktop/Project-BMFG'
-    model_checkpoint = glob_path + '/checkpoints/mtts_sys_kernal' + kernal + '_' + dataset_type + '_drop2_nb256.hdf5'
     batch_size = 32
+    path = 'C:/Users/Zed/Desktop/Project-BMFG'
+    model_checkpoint = path + '/checkpoints/mtts_sys_kernal' + kernal + '_' + dataset_type + '_drop2_nb256.hdf5'
 
-    video_folder_path = str()
-    if data_type != 'test':
-        video_folder_path = glob_path + '/Phase1_data/Videos/' + data_type + '/'
-    else:
-        video_folder_path = glob_path + '/Phase2_data/Videos/' + data_type + '/'
+    # model_checkpoint = glob_path + '/checkpoints/mtts_sys_kernal' + kernal + '_' + dataset_type + '_drop2_nb256.hdf5'
+    # glob_path = 'C:/Users/Zed/Desktop/Project-BMFG'
+    # # video_folder_path = str()
+    # if data_type != 'test':
+    #     video_folder_path = glob_path + '/Phase1_data/Videos/' + data_type + '/'
+    # else:
+    #     video_folder_path = glob_path + '/Phase2_data/Videos/' + data_type + '/'
+    #
+    # video_path = []
+    # for path in sorted(os.listdir(video_folder_path)):
+    #     if os.path.isfile(os.path.join(video_folder_path, path)):
+    #         video_path.append(path)
+    # sample_video_name = video_path[random.randint(0, len(video_path))][0:-4]
+    # sample_video_path = video_folder_path + sample_video_name + '.mkv'
+    # dXsub = preprocess_raw_video(sample_video_path, dim=48)
+    # dXsub_len = (dXsub.shape[0] // frame_depth) * frame_depth
+    # dXsub = dXsub[:dXsub_len, :, :, :]
+    #
+    # BP_folder_path = str()
+    # if data_type != 'train':
+    #     BP_folder_path = glob_path + '/Phase2_data/blood_pressure/' + data_type + '_set_bp/'
+    # else:
+    #     BP_folder_path = glob_path + '/Phase1_data/Ground_truth/BP_raw_1KHz/'
+    #
+    # BP_sample_name = sample_video_name[0:4] + '-' + sample_video_name[5:8]
+    # print(BP_sample_name)
+    # if '.' in BP_sample_name:
+    #     BP_sample_name += 'txt'
+    # else:
+    #     BP_sample_name += '.txt'
+    #
+    # sample_BP_file = np.loadtxt(BP_folder_path + BP_sample_name)
+    # size = sample_BP_file.shape[0]
+    # BP_systolic, _ = find_peaks(sample_BP_file, distance=10)
+    # BP_inter = np.zeros(sample_BP_file.shape[0])
+    # prev_index = 0
+    # for index in BP_systolic:
+    #     y_interp = interp1d([prev_index, index], [sample_BP_file[prev_index], sample_BP_file[index]])
+    #     for i in range(prev_index, index + 1):
+    #         BP_inter[i] = y_interp(i)
+    #     prev_index = index
+    # y_interp = interp1d([prev_index, size - 1], [sample_BP_file[prev_index], sample_BP_file[size - 1]])
+    # for i in range(prev_index, size):
+    #     BP_inter[i] = y_interp(i)
+    #
+    # BP_gt = BP_inter[0:dXsub_len]
 
-    video_path = []
-    for path in sorted(os.listdir(video_folder_path)):
-        if os.path.isfile(os.path.join(video_folder_path, path)):
-            video_path.append(path)
-    sample_video_name = video_path[random.randint(0, len(video_path))][0:-4]
-    sample_video_path = video_folder_path + sample_video_name + '.mkv'
-    dXsub = preprocess_raw_video(sample_video_path, dim=48)
-    dXsub_len = (dXsub.shape[0] // frame_depth) * frame_depth
-    dXsub = dXsub[:dXsub_len, :, :, :]
-
-    BP_folder_path = str()
-    if data_type != 'train':
-        BP_folder_path = glob_path + '/Phase2_data/blood_pressure/' + data_type + '_set_bp/'
-    else:
-        BP_folder_path = glob_path + '/Phase1_data/Ground_truth/BP_raw_1KHz/'
-
-    BP_sample_name = sample_video_name[0:4] + '-' + sample_video_name[5:8]
-    print(BP_sample_name)
-    if '.' in BP_sample_name:
-        BP_sample_name += 'txt'
-    else:
-        BP_sample_name += '.txt'
-
-    sample_BP_file = np.loadtxt(BP_folder_path + BP_sample_name)
-    size = sample_BP_file.shape[0]
-    BP_systolic, _ = find_peaks(sample_BP_file, distance=10)
-    BP_inter = np.zeros(sample_BP_file.shape[0])
-    prev_index = 0
-    for index in BP_systolic:
-        y_interp = interp1d([prev_index, index], [sample_BP_file[prev_index], sample_BP_file[index]])
-        for i in range(prev_index, index + 1):
-            BP_inter[i] = y_interp(i)
-        prev_index = index
-    y_interp = interp1d([prev_index, size - 1], [sample_BP_file[prev_index], sample_BP_file[size - 1]])
-    for i in range(prev_index, size):
-        BP_inter[i] = y_interp(i)
-
-    BP_gt = BP_inter[0:dXsub_len]
-
-    # dXsub = np.load(path + '/preprocessed_v4v/' + data_type + '_frames_' + dataset_type + '.npy')
-    # BP_gt = np.load(path + '/preprocessed_v4v/' + data_type + '_BP_mean_systolic.npy')
-    # size = dXsub.shape[0]
-    # print('*************', size, '*************')
-    # print(BP_gt.shape)
-    # x = random.randint(0, size - 25 * 500 - 1)
-    # dXsub = dXsub[x:x + 25 * 500]
-    # BP_gt = BP_gt[x:x + 25 * 500]
+    dXsub = np.load(path + '/preprocessed_v4v/' + data_type + '_frames_' + dataset_type + '.npy')
+    BP_gt = np.load(path + '/preprocessed_v4v/' + data_type + '_BP_mean_systolic.npy')
+    size = dXsub.shape[0]
+    print('*************', size, '*************')
+    print(BP_gt.shape)
+    x = random.randint(0, size - 25 * 500 - 1)
+    dXsub = dXsub[x:x + 25 * 500]
+    BP_gt = BP_gt[x:x + 25 * 500]
 
     model = MTTS_CAN(frame_depth, 32, 64, (img_rows, img_cols, 3))
     model.load_weights(model_checkpoint)
@@ -122,7 +124,7 @@ def my_predict(data_type, dataset_type, kernal):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-data', '--data_type', type=str, default='valid',
+    parser.add_argument('-data', '--data_type', type=str, default='train',
                         help='data type')
     parser.add_argument('-dataset', '--dataset_type', type=str, default='face_large',
                         help='dataset type')
