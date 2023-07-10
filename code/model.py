8bfca564d5143a7601f6e1ae07e8607da6ba182c
ZechenZed <fluffyzed2@gmail.com>
2023-07-10 15:50:56 -0400

    delete some random error

--- code/model.py
+++ code/model.py
@@ -88,21 +88,69 @@ class TSM(tf.keras.layers.Layer):
         return config
 
 
-def TSM_Cov2D(x, n_frame, nb_filters=128, kernel_size=(3, 3), activation='tanh', padding='same'):
-    x = TSM()(x, 25, n_frame)
+def TSM_Cov2D(x, n_video, n_frame, nb_filters=128, kernel_size=(3, 3), activation='tanh', padding='same'):
+    x = TSM()(x, n_video, n_frame)
     x = Conv2D(nb_filters, kernel_size, padding=padding, activation=activation)(x)
     return x
 
 
 # %%
 # %% MTTS-CAN
-def MTTS_CAN(n_frame, nb_filters1, nb_filters2, input_shape, kernel_size=(3, 3), dropout_rate1=0.25,
+# def MTTS_CAN(n_frame, nb_filters1, nb_filters2, input_shape, kernel_size=(3, 3), dropout_rate1=0.25,
+#              dropout_rate2=0.5, pool_size=(2, 2), nb_dense=128):
+#     diff_input = Input(shape=input_shape)
+#     rawf_input = Input(shape=input_shape)
+#
+#     d1 = TSM_Cov2D(diff_input, n_frame, nb_filters1, kernel_size, padding='same', activation='tanh')
+#     d2 = TSM_Cov2D(d1, n_frame, nb_filters1, kernel_size, padding='valid', activation='tanh')
+#
+#     r1 = Conv2D(nb_filters1, kernel_size, padding='same', activation='tanh')(rawf_input)
+#     r2 = Conv2D(nb_filters1, kernel_size, activation='tanh')(r1)
+#
+#     g1 = Conv2D(1, (1, 1), padding='same', activation='sigmoid')(r2)
+#     g1 = Attention_mask()(g1)
+#     gated1 = multiply([d2, g1])
+#
+#     d3 = AveragePooling2D(pool_size)(gated1)
+#     d4 = Dropout(dropout_rate1)(d3)
+#
+#     r3 = AveragePooling2D(pool_size)(r2)
+#     r4 = Dropout(dropout_rate1)(r3)
+#
+#     d5 = TSM_Cov2D(d4, n_frame, nb_filters2, kernel_size, padding='same', activation='tanh')
+#     d6 = TSM_Cov2D(d5, n_frame, nb_filters2, kernel_size, padding='valid', activation='tanh')
+#
+#     r5 = Conv2D(nb_filters2, kernel_size, padding='same', activation='tanh')(r4)
+#     r6 = Conv2D(nb_filters2, kernel_size, activation='tanh')(r5)
+#
+#     g2 = Conv2D(1, (1, 1), padding='same', activation='sigmoid')(r6)
+#     g2 = Attention_mask()(g2)
+#     gated2 = multiply([d6, g2])
+#
+#     d7 = AveragePooling2D(pool_size)(gated2)
+#     d8 = Dropout(dropout_rate1)(d7)
+#
+#     d9 = Flatten()(d8)
+#
+#     d10_y = Dense(nb_dense, activation='tanh')(d9)
+#     d11_y = Dropout(dropout_rate2)(d10_y)
+#     out_y = Dense(1, name='output_1')(d11_y)
+#
+#     # d10_r = Dense(nb_dense, activation='tanh')(d9)
+#     # d11_r = Dropout(dropout_rate2)(d10_r)
+#     # out_r = Dense(1, name='output_2')(d11_r)
+#
+#     # model = Model(inputs=[diff_input, rawf_input], outputs=[out_y, out_r])
+#     model = Model(inputs=[diff_input, rawf_input], outputs=[out_y])
+#     return model
+
+def MTTS_CAN(n_videos, n_frame, nb_filters1, nb_filters2, input_shape, kernel_size=(3, 3), dropout_rate1=0.25,
              dropout_rate2=0.5, pool_size=(2, 2), nb_dense=128):
     diff_input = Input(shape=input_shape)
     rawf_input = Input(shape=input_shape)
 
-    d1 = TSM_Cov2D(diff_input, n_frame, nb_filters1, kernel_size, padding='same', activation='tanh')
-    d2 = TSM_Cov2D(d1, n_frame, nb_filters1, kernel_size, padding='valid', activation='tanh')
+    d1 = TSM_Cov2D(diff_input, n_videos, n_frame, nb_filters1, kernel_size, padding='same', activation='tanh')
+    d2 = TSM_Cov2D(d1, n_videos, n_frame, nb_filters1, kernel_size, padding='valid', activation='tanh')
 
     r1 = Conv2D(nb_filters1, kernel_size, padding='same', activation='tanh')(rawf_input)
     r2 = Conv2D(nb_filters1, kernel_size, activation='tanh')(r1)
@@ -117,8 +165,8 @@ def MTTS_CAN(n_frame, nb_filters1, nb_filters2, input_shape, kernel_size=(3, 3),
     r3 = AveragePooling2D(pool_size)(r2)
     r4 = Dropout(dropout_rate1)(r3)
 
-    d5 = TSM_Cov2D(d4, n_frame, nb_filters2, kernel_size, padding='same', activation='tanh')
-    d6 = TSM_Cov2D(d5, n_frame, nb_filters2, kernel_size, padding='valid', activation='tanh')
+    d5 = TSM_Cov2D(d4, n_videos, n_frame, nb_filters2, kernel_size, padding='same', activation='tanh')
+    d6 = TSM_Cov2D(d5, n_videos, n_frame, nb_filters2, kernel_size, padding='valid', activation='tanh')
 
     r5 = Conv2D(nb_filters2, kernel_size, padding='same', activation='tanh')(r4)
     r6 = Conv2D(nb_filters2, kernel_size, activation='tanh')(r5)
