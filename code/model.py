de1f985aae001468a666094f3fab7c6ace2da755
ZechenZed <fluffyzed2@gmail.com>
2023-07-10 15:47:16 -0400

    delete some random error

--- code/model.py
+++ code/model.py
@@ -25,30 +25,61 @@ class Attention_mask(tf.keras.layers.Layer):
         return config
 
 
+# class TSM(tf.keras.layers.Layer):
+#     def call(self, x, n_frame, fold_div=3):
+#         nt, h, w, c = x.shape
+#         x = K.reshape(x, (-1, n_frame, h, w, c))
+#         fold = c // fold_div
+#         last_fold = c - (fold_div - 1) * fold
+#         out1, out2, out3 = tf.split(x, [fold, fold, last_fold], axis=-1)
+#
+#         # Shift left
+#         padding_1 = tf.zeros_like(out1)
+#         padding_1 = padding_1[:, -1, :, :, :]
+#         padding_1 = tf.expand_dims(padding_1, 1)
+#         _, out1 = tf.split(out1, [1, n_frame - 1], axis=1)
+#         out1 = tf.concat([out1, padding_1], axis=1)
+#
+#         # Shift right
+#         padding_2 = tf.zeros_like(out2)
+#         padding_2 = padding_2[:, 0, :, :, :]
+#         padding_2 = tf.expand_dims(padding_2, 1)
+#         out2, _ = tf.split(out2, [n_frame - 1, 1], axis=1)
+#         out2 = tf.concat([padding_2, out2], axis=1)
+#
+#         out = tf.concat([out1, out2, out3], axis=-1)
+#         out = K.reshape(out, (-1, h, w, c))
+#
+#         return out
+#
+#     def get_config(self):
+#         config = super(TSM, self).get_config()
+#         return config
+
 class TSM(tf.keras.layers.Layer):
-    def call(self, x, n_frame, fold_div=3):
-        nt, _, h, w, c = x.shape
-        x = K.reshape(x, (-1, nt, n_frame, h, w, c))
+    def call(self, x, n_videos, n_frame, fold_div=3):
+        _, h, w, c = x.shape
+        x = tf.reshape(x, (-1, n_videos, n_frame, h, w, c))
         fold = c // fold_div
         last_fold = c - (fold_div - 1) * fold
         out1, out2, out3 = tf.split(x, [fold, fold, last_fold], axis=-1)
 
         # Shift left
         padding_1 = tf.zeros_like(out1)
-        padding_1 = padding_1[:, -1, :, :, :]
-        padding_1 = tf.expand_dims(padding_1, 1)
-        _, out1 = tf.split(out1, [1, n_frame - 1], axis=1)
-        out1 = tf.concat([out1, padding_1], axis=1)
+        padding_1 = padding_1[:, :, -1, :, :, :]
+        padding_1 = tf.expand_dims(padding_1, 2)
+        _, out1 = tf.split(out1, [1, n_frame - 1], axis=2)
+        out1 = tf.concat([out1, padding_1], axis=2)
 
         # Shift right
         padding_2 = tf.zeros_like(out2)
-        padding_2 = padding_2[:, 0, :, :, :]
-        padding_2 = tf.expand_dims(padding_2, 1)
-        out2, _ = tf.split(out2, [n_frame - 1, 1], axis=1)
-        out2 = tf.concat([padding_2, out2], axis=1)
+        padding_2 = padding_2[:, :, 0, :, :, :]
+        padding_2 = tf.expand_dims(padding_2, 2)
+        out2, _ = tf.split(out2, [n_frame - 1, 1], axis=2)
+        out2 = tf.concat([padding_2, out2], axis=2)
 
         out = tf.concat([out1, out2, out3], axis=-1)
-        out = K.reshape(out, (-1, h, w, c))
+        out = tf.reshape(out, (-1, h, w, c))
 
         return out
 
@@ -58,7 +89,7 @@ class TSM(tf.keras.layers.Layer):
 
 
 def TSM_Cov2D(x, n_frame, nb_filters=128, kernel_size=(3, 3), activation='tanh', padding='same'):
-    x = TSM()(x, n_frame)
+    x = TSM()(x, 25, n_frame)
     x = Conv2D(nb_filters, kernel_size, padding=padding, activation=activation)(x)
     return x
 
