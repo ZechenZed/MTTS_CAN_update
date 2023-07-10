3de9d11b63d1c2722efd07598e195a4685dde1be
ZechenZed <fluffyzed2@gmail.com>
2023-07-10 09:31:23 -0400

    delete 2 dropout layers

--- code/my_train.py
+++ code/my_train.py
@@ -14,7 +14,7 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from inference_preprocess import preprocess_raw_video, count_frames
 from model import MTTS_CAN, MT_CAN_3D
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 
 # BP --> 25 Hz
